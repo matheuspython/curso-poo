86c925cfd0fd386ae2f1943f520fd860e3898da3
matheuspython <ruivorobado@gmail.com>
2021-12-07 22:23:52 -0300

    metodos, classes, atributos e instancias criados

--- Caneta.java
+++ Caneta.java
@@ -5,13 +5,35 @@ public class Caneta {
   int carga;
   boolean tampada;
 
-  void rabiscar(){
-    
+  void staus() {
+    String estaTampada;
+    if(tampada){
+      estaTampada = "sim tampada";
+    }else{
+      estaTampada = "naõ ela esta destampada";
+    }
+    System.out.print("--------------------------");
+      System.out.print("Modelo: " + this.modelo + "\n");
+      System.out.print("Uma caneta " + this.cor + "\n");
+      System.out.print("Ponta: " + this.ponta + "\n");
+      System.out.print("carga: " + this.carga + "\n");
+      System.out.print("esta tampada? " + estaTampada + "\n");
+    System.out.print("--------------------------");
   }
-  void tampar(){
 
+  void rabiscar(){
+    if(this.tampada){
+    System.out.print("n da ela ta tampada \n");
+    return; 
+    }
+    System.out.print("Rabiscando \n");
   }
+
+  void tampar(){
+    this.tampada = true;
+  }
+  
   void destampar(){
-    
+    this.tampada = false;
   }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 class Main {
   public static void main(String[] args) {
+    Caneta caneta1 = new Caneta();    
+    caneta1.cor = "azul";
+    caneta1.ponta = 0.5f;
+    caneta1.tampada = false;
+    // caneta1.staus();
+    // caneta1.tampar();
+    // caneta1.staus();
+    caneta1.rabiscar();
+    caneta1.tampar();
+    caneta1.rabiscar();
     
   }
 }
