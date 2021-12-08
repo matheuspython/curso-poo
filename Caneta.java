4a0b16facff5f558bd0a6b02b3192caabce53761
matheuspython <ruivorobado@gmail.com>
2021-12-07 22:02:15 -0300

    primeira classe criada

---
+++ Caneta.java
@@ -0,0 +1,17 @@
+public class Caneta {
+  String modelo;
+  String cor;
+  float ponta;
+  int carga;
+  boolean tampada;
+
+  void rabiscar(){
+    
+  }
+  void tampar(){
+
+  }
+  void destampar(){
+    
+  }
+}
